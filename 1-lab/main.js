883a77331c3cc7761e876aee8c393a53b1274de6
iziomoziku <iziomoziku@yahoo.com>
2022-03-08 13:49:15 -0500

    fixed main js

--- 1-lab/main.js
+++ 1-lab/main.js
@@ -1,14 +1,25 @@
 let xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    $("#btn").click(function () {
-      // console.log(this.responseText);
-      document.getElementById("demo").innerHTML = this.responseText;
-    });
-    // document.getElementById("demo").innerHTML = this.responseText;
-  }
-};
+// xhttp.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     $("#btn").click(function () {
+//       // console.log(this.responseText);
+//       document.getElementById("demo").innerHTML = this.responseText;
+//     });
+//     // document.getElementById("demo").innerHTML = this.responseText;
+//   }
+// };
 
-xhttp.open("GET", "data.txt", true);
-xhttp.send();
+function loadDoc() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "data.txt", true);
+  xhttp.send();
+}
+
+// xhttp.open("GET", "data.txt", true);
+// xhttp.send();

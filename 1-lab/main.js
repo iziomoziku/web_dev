36eeb146cfc78f6d45dbaccef85a7f89a42095e2
iziomoziku <iziomoziku@yahoo.com>
2022-03-08 15:06:25 -0500

    fix main js

--- 1-lab/main.js
+++ 1-lab/main.js
@@ -14,12 +14,24 @@ function loadDoc() {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+      // document.getElementById("demo").innerHTML = this.responseText;
+      console.log(this.responseText);
     }
   };
   xhttp.open("GET", "data.txt", true);
   xhttp.send();
 }
+
+// $("button").click(function () {
+//   $("#div1").load("demo_test.txt", function (responseTxt, statusTxt, xhr) {
+//     if (statusTxt == "success") {
+//       alert("External content loaded successfully!");
+//     }
+//     if (statusTxt == "error") {
+//       alert("Error: " + xhr.status + ": " + xhr.statusText);
+//     }
+//   });
+// });
 
 // xhttp.open("GET", "data.txt", true);
 // xhttp.send();

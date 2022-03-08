4ba21b49e269f1b5fe5c79319b099251bf5779ba
iziomoziku <iziomoziku@yahoo.com>
2022-03-08 13:41:24 -0500

    fixed error in js file

--- 1-lab/main.js
+++ 1-lab/main.js
@@ -3,7 +3,8 @@ let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     $("#btn").click(function () {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+      document.getElementById("demo").innerHTML = this.responseText;
     });
     // document.getElementById("demo").innerHTML = this.responseText;
   }
